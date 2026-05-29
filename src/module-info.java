b5468219717be16771b52c2376247587e5da75e8
LEONARDO SEBASTIAN SOLORZANO CHAVARRIA <lsolorzanoc1@miumg.edu.gt>
2026-05-28 20:57:33 -0600

    Semana 1: Estructura del proyecto y clase HolaMundo

---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module POO_Java_Roadmap {
+}
---
+++ src/semana01_fundamentos/HolaMundo.java
@@ -0,0 +1,8 @@
+package semana01_fundamentos;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Inicio del Roadmap de Java - POO");
+        System.out.println("Autor: Leonardo Sebastian Solorzano Chavarria");
+    }
+}
